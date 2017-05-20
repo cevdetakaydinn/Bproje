a02db0f107862ea26153ac6cf16e7f8a62be08fa
cevdetakaydinn <cevdet.akaydin@bil.omu.edu.tr>
2017-05-20 10:44:26 +0300

    pop grades

--- app/controllers/weeklyschedules_controller.rb
+++ app/controllers/weeklyschedules_controller.rb
@@ -56,7 +56,7 @@ class WeeklyschedulesController < ApplicationController
       end
     else
       generatePopulation(@curriculum)
-      render :json =>@population.calcFitness
+      # render :json =>@population.calcFitness
       # @population.naturalSelection()
       # @population.generate()#crossover
       # @population.evaluate();
@@ -66,9 +66,9 @@ class WeeklyschedulesController < ApplicationController
   #Ders programı Uretme
   def generatePopulation(curriculum)
     #Mufredatı kullanarak random population olustur
-    @population = Population.new(1,2,curriculum)
+    @population = Population.new(1,1,curriculum)
     @array =  @population.getpop(0)
-    # render :json => @population
+    render :json => @population
   end
 
   def saveSchedule
--- lib/dna.rb
+++ lib/dna.rb
@@ -5,26 +5,38 @@ class Dna
   #@@midpoint
   def initialize(curriculum,day,hr)
     rnd=Random.new
-    @genes=Array.new(day){Array.new(hr)}
+    @Chromosome= Hash.new
+    @highestGrade=Departmentlesson.maximum(:grade)
+    i = 1
+    while i<=@highestGrade
+      @Chromosome[i]=Array.new(day){Array.new(hr)}
+      i=i+1
+    end
 
     curriculum.each do |cur|
-      hour= Departmentlesson.find(cur[0].departmentlesson_id).hour_amount
-      curCurrent=cur[0].id
-      i=0
-      while i < hour
-        rday=rnd.rand(day)
-        rhour=rnd.rand(hr)
-          if @genes[rday][rhour].blank?
-            @genes[rday][rhour]=curCurrent
-            i=i+1
-          end
+      curCurrent=cur.first
+      if !(curCurrent.nil?)
+        dp = Departmentlesson.find(curCurrent.departmentlesson_id)
+        hour = dp.hour_amount
+        currentGrade = dp.grade
+
+        i=0
+        while i < hour
+          rday=rnd.rand(day)
+          rhour=rnd.rand(hr)
+            if @Chromosome[currentGrade][rday][rhour].blank?
+              @Chromosome[currentGrade][rday][rhour]=curCurrent.id
+              i=i+1
+            end
+        end
       end
+
     end
     # @genes=Matrix.build(Day.all.size, Lessonhour.all.size) {|row, col| curriculum[rand((Lessonhour.all.size*Day.all.size)-curriculum.size)] }
   end
 
   def getGen
-    @genes
+    @Chromosome
   end
   def crossover(partner)
 
@@ -39,23 +51,24 @@ class Dna
     @array=Array.new
     self.getGen.each_with_index do |gen,row|
       gen.each_with_index do |modul,col|
-        # @array<<row
-        # @array<<"/"
-        # @array<<col
         if !(modul.blank?)
           #Aynı saatteki bütün dersleri bul
           @ws = Weeklyschedule.where(day_id:row , lessonhours_id:col)
           @cur=Curriculum.find(modul)
-          if #Hoca çakışması yoksa
-            @score = @score + 1
-          elsif #Sınıf çakışması yoksa
-            @score = @score + 1
-          elsif #Aynı sınıfın başka dersi yoksa
-            @score = @score + 1
+
+          @ws.each do |ws|
+              #hoca çakışması kontrol
+              if @cur.admin.id != ws.curriculum.admin.id
+                @score = @score + 1
+              # elsif #Sınıf çakışması yoksa ve sınıf kontenjanı yetiorsa
+              #    @score = @score + 1
+              # elsif #Aynı sınıfın başka dersi yoksa
+              #   @score = @score + 1
+              end
           end
         end
       end
     end
-    return @array
+    return @score
   end
 end
